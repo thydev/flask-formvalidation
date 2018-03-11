76fe9429df5460a0fbd3df0baf38e9fc6ddaedab
Chanthy Ngin <nginchanthy@gmail.com>
2018-03-10 18:34:49 -0800

    Added data from registration form to mysql

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request, session, flash, redirect
-import re, datetime
+import re, datetime, md5, os, binascii
+from mysqlconnection import MySQLConnector
+
 app = Flask(__name__)
 app.secret_key = "fs@#dSdsf132SF$#$@#()9dx"
+mysql = MySQLConnector(app, 'friendships')
+
 # create a regular expression object that we can use run operations on
 email_reg = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-pwd_reg = re.compile('^(?=\S{3,20}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])')
+pwd_reg = re.compile('^(?=\S{8,20}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])')
 
 
 @app.route('/')
 def index():
+    if 'loggedin' not in session:
+        session['loggedin'] = {
+            'id': -1,
+            'name': "undefined"
+        }
     return render_template('index.html')
 
-@app.route('/', methods=["POST"])
-def result():
+@app.route('/create', methods=["POST"])
+def create():
     # print request.form
     isValid = True
     firstname = request.form['firstname'].strip()
@@ -21,13 +30,22 @@ def result():
     email = request.form['email']
     password = request.form['password']
     password_confirm = request.form['password_confirm']
-    birth_date = request.form['birthdate']
 
-    if not (len(firstname) > 0):
-        flash("First name can not be blank!", "error")
+    salt =  binascii.b2a_hex(os.urandom(15))
+    hashed_pw = md5.new(password + salt).hexdigest()
+    # birth_date = request.form['birthdate']
+
+    # 1. First Name - letters only, at least 2 characters and that it was submitted
+    # 2. Last Name - letters only, at least 2 characters and that it was submitted
+    # 3. Email - Valid Email format, and that it was submitted
+    # 4. Password - at least 8 characters, and that it was submitted
+    # 5. Password Confirmation - matches password
+
+    if not (len(firstname) > 1):
+        flash("First Name - letters only, at least 2 characters!", "error")
         isValid = False
-    if not (len(lastname) > 0):
-        flash("Last name can not be blak!", "error")
+    if not (len(lastname) > 1):
+        flash("Last Name - letters only, at least 2 characters!", "error")
         isValid = False
     if not email_reg.match(email):
         flash("Invalid Email Address!", "error")
@@ -36,28 +54,47 @@ def result():
         flash("Please input your password", "error")
         isValid = False
     elif not pwd_reg.match(password):
-        flash("Password must contain at least 1 uppercase, 1 lowercase, 1 number and 1 symbol", "error")
+        flash("Password must contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 symbol", "error")
         isValid = False
     elif password != password_confirm:
         flash("Password does not match!", "error")
         isValid = False
 
-    if not birth_date:
-        flash("Please input your birth date!", "error")
-        isValid = False
-    else:
-        # birth_date = datetime.datetime.strptime(birth_date, "%Y-%m-%d")
-        present = datetime.datetime.now().strftime("%Y-%m-%d")
-        print birth_date, present, birth_date > present
-        if birth_date >= present:
-            flash("Birth date can not be in the future!", "error")
-            isValid = False
+    # if not birth_date:
+    #     flash("Please input your birth date!", "error")
+    #     isValid = False
+    # else:
+    #     # birth_date = datetime.datetime.strptime(birth_date, "%Y-%m-%d")
+    #     present = datetime.datetime.now().strftime("%Y-%m-%d")
+    #     print birth_date, present, birth_date > present
+    #     if birth_date >= present:
+    #         flash("Birth date can not be in the future!", "error")
+    #         isValid = False
     
     if isValid:
-        flash("Thank you for submitting information!", "sucess")
+        query = "INSERT INTO users (first_name, last_name, email, password, salt, created_at, updated_at) " 
+        query += "VALUES (:first_name, :last_name, :email, :password, :salt, NOW(), NOW())"
+        data = { 
+            'first_name': firstname,
+            'last_name': lastname,
+            'email': email,
+            'password': hashed_pw,
+            'salt': salt
+            }
+
+        user_id = mysql.query_db(query, data)
+        if 'loggedin' in session:
+            session['loggedin'] = {
+                'id': user_id,
+                'name': firstname + ' ' + lastname
+            }
+        print session['loggedin']
+        flash("Registered sucessfully!", "sucess")
         # return render_template('result.html', fullname = fullname, location = location, language = language, comment = comment )
         return redirect('/')
     else:
         return redirect('/')
+
+
 
 app.run(debug = True)
